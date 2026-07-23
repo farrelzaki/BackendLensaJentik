a9b461e9fb3da34e1a380412e3f894dbdbd5b1e7
farrelzaki <farrelmzaki77@gmail.com>
2026-07-24 00:02:24 +0700

    tes deployment

--- lensajentik-backend/routes/web.php
+++ lensajentik-backend/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Database connected: ' . DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        return 'Connection failed: ' . $e->getMessage();
+    }
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
